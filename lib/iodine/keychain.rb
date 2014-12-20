70561d7c924f3a4b0bb6a179044cd1d61f16911b
Asmod4n <Asmod4n@users.noreply.github.com>
2014-12-20 17:32:12 +0100

    delete identities from keychain if file was deleted

--- lib/iodine/keychain.rb
+++ lib/iodine/keychain.rb
@@ -4,8 +4,8 @@ module Iodine
   class Keychain
     def initialize(directory)
       @directory = directory
-      @mtime = File.mtime(@directory)
       @identities = {}
+      @mtime = File.mtime(@directory)
       Dir.glob(File.join(@directory, '*.id')) do |id_file|
         @identities[File.basename(id_file, '.id')] = CZMQ::Zconfig.load(id_file)
       end
@@ -17,14 +17,25 @@ module Iodine
         load_ids
       end
 
-      if (id = @identities[identity])
-        id.reload if id.has_changed
-        id.resolve(path, nil)
+      if @identities[identity]
+        if @identities[identity].has_changed
+          id_file = File.join(@directory, "#{identity}.id")
+          unless File.dirname(id_file) == @directory
+            fail ArgumentError, "identity=#{identity} contains illegal characters", caller
+          end
+          @identities[identity] = CZMQ::Zconfig.load(id_file)
+        end
+
+        @identities[identity].resolve(path, nil)
       end
     end
 
     def store(identity, path, value)
       id_file = File.join(@directory, "#{identity}.id")
+      unless File.dirname(id_file) == @directory
+        fail ArgumentError, "identity=#{identity} contains illegal characters", caller
+      end
+
       unless (id = @identities[identity])
         File.new(id_file, 'w+').close
         id = @identities[identity] = CZMQ::Zconfig.load(id_file)
@@ -32,18 +43,27 @@ module Iodine
 
       id.put(path, value)
       id.save(id_file)
+      @mtime = File.mtime(@directory)
       self
     end
 
     def load_ids
+      identities = []
       Dir.glob(File.join(@directory, '*.id')) do |id_file|
-        identity = File.basename(id_file, '.id')
-        if (id = @identities[identity])
-          id.reload if id.has_changed
+        identities << File.basename(id_file, '.id')
+        if @identities[identities.last]
+          if @identities[identities.last].has_changed
+            @identities[identities.last] = CZMQ::Zconfig.load(id_file)
+          end
         else
-          @identities[identity] = CZMQ::Zconfig.load(id_file)
+          @identities[identities.last] = CZMQ::Zconfig.load(id_file)
         end
       end
+
+      (@identities.keys - identities).each {|identity| @identities.delete(identity) }
+      nil
     end
   end
+
+  Keychain.freeze
 end
