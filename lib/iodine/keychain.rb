36f2e74b94550e17791f45e610574306b377c875
Asmod4n <Asmod4n@users.noreply.github.com>
2014-12-21 17:47:25 +0100

    add user authentication and encryption

--- lib/iodine/keychain.rb
+++ lib/iodine/keychain.rb
@@ -1,18 +1,35 @@
-﻿require 'czmq/zconfig'
-
-module Iodine
+﻿module Iodine
   class Keychain
     def initialize(directory = 'identities')
-      @directory = directory
+      @directory = directory.dup
       @identities = {}
+      @keychain = {}
       @mtime = File.mtime(@directory)
       Dir.glob(File.join(@directory, '*.id')) do |id_file|
         @identities[File.basename(id_file, '.id')] = CZMQ::Zconfig.load(id_file)
       end
     end
 
+    def reload_ids
+      @mtime = File.mtime(@directory)
+      identities = []
+      Dir.glob(File.join(@directory, '*.id')) do |id_file|
+        identities << File.basename(id_file, '.id')
+        if (id = @identities[identities.last])
+          if id.has_changed
+            @identities[identities.last] = CZMQ::Zconfig.load(id_file)
+          end
+        else
+          @identities[identities.last] = CZMQ::Zconfig.load(id_file)
+        end
+      end
+
+      (@identities.keys - identities).each {|identity| @identities.delete(identity) }
+      nil
+    end
+
     def lookup(identity, path)
-      load_ids if File.mtime(@directory) != @mtime
+      reload_ids if File.mtime(@directory) != @mtime
 
       if (id = @identities[identity])
         if id.has_changed
@@ -40,26 +57,75 @@ module Iodine
 
       id.put(path, value)
       id.save(id_file)
-      @mtime = File.mtime(@directory)
       self
     end
 
-    def load_ids
-      @mtime = File.mtime(@directory)
-      identities = []
-      Dir.glob(File.join(@directory, '*.id')) do |id_file|
-        identities << File.basename(id_file, '.id')
-        if (id = @identities[identities.last])
-          if id.has_changed
-            @identities[identities.last] = CZMQ::Zconfig.load(id_file)
+    def auth(identity, password)
+      salt = Crypto::PwHash::ScryptSalsa208SHA256.salt
+      secret_key = Crypto::PwHash.scryptsalsa208sha256(Crypto::Auth::KEYBYTES, password, salt)
+      mac = Crypto.auth(password, secret_key)
+      store(identity, '/auth/salt', Sodium.bin2hex(salt))
+      store(identity, '/auth/mac', Sodium.bin2hex(mac))
+      @keychain[identity] = secret_key
+      [salt, mac]
+    ensure
+      password.clear
+    end
+
+    def verify(identity, password)
+      salt = lookup(identity, '/auth/salt')
+      if salt
+        mac = lookup(identity, '/auth/mac')
+        if mac
+          unless (secret_key = @keychain[identity])
+            Sodium.hex2bin!(salt, Crypto::PwHash::ScryptSalsa208SHA256::SALTBYTES)
+            secret_key = Crypto::PwHash.scryptsalsa208sha256( Crypto::Auth::KEYBYTES,
+                                                              password,
+                                                              salt)
           end
-        else
-          @identities[identities.last] = CZMQ::Zconfig.load(id_file)
+
+          Sodium.hex2bin!(mac, Crypto::Auth::BYTES)
+          if Crypto::Auth.verify( mac,
+                                  password,
+                                  secret_key)
+            @keychain[identity] ||= secret_key
+          end
         end
       end
+    ensure
+      password.clear
+    end
 
-      (@identities.keys - identities).each {|identity| @identities.delete(identity) }
-      nil
+    def logout(identity)
+      @keychain.delete(identity)
+      true
+    end
+
+    def encrypt(identity, password, path, value)
+      if (secret_key = verify(identity, password))
+        nonce = Crypto::SecretBox.nonce
+        ciphertext = Crypto.secretbox(value, nonce, secret_key)
+        store(identity, path, Sodium.bin2hex(ciphertext))
+        store(identity, "#{path}/nonce", Sodium.bin2hex(nonce))
+        [nonce, ciphertext]
+      end
+    end
+
+    def decrypt(identity, password, path)
+      if (secret_key = verify(identity, password))
+        ciphertext = lookup(identity, path)
+        if ciphertext
+          nonce = lookup(identity, "#{path}/nonce")
+          if nonce
+            Sodium.hex2bin!(ciphertext, ciphertext.bytesize / 2)
+            Sodium.hex2bin!(nonce, Crypto::SecretBox::NONCEBYTES)
+            Crypto::SecretBox.open!(  ciphertext,
+                                      nonce,
+                                      secret_key,
+                                      Encoding.default_external)
+          end
+        end
+      end
     end
   end
 
